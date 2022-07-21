990fde78a4a72c78c6917f6004276c0992d1f363
avina <avina@LAPTOP-MF81IFK9>
2022-07-21 13:16:18 +0530

    [Avinash]:added new demo1

---
+++ src/Sample1/Demo1.java
@@ -0,0 +1,16 @@
+package Sample1;
+
+public class Demo1 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hi");
+		
+		System.out.println("Hi");
+		
+		System.out.println("Hello");
+		
+		System.out.println("Hello");
+	}
+
+}
